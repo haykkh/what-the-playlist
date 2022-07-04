82554ac3c7485e355851092a7170417d78abc71d
Hayk Khachatryan <hi@hayk.io>
2022-07-03 21:08:05 -0400

    feat: hello [pages/callback]

---
+++ pages/callback.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="callback">
+    <div class="callback-alert">
+      Loading
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth"
+
+const route = useRoute()
+
+const authStore = useAuthStore()
+
+if (typeof route.query.code === "string") {
+  authStore.fetchAccessToken(route.query.code).then(() => { navigateTo("/") })
+}
+</script>
+
+<style lang="scss">
+.callback {
+  @apply flex justify-center items-center min-h-screen;
+
+  &-alert {
+    @apply alert alert-info w-fit;
+  }
+}
+</style>
