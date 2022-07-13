bb971a90e709904098d8a5c29dccd87e3185fadc
Hayk Khachatryan <hi@hayk.io>
2022-07-13 19:34:13 -0400

    feat: page for playlist index

---
+++ pages/playlists/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <PlaylistTable />
+</template>
+
+<script setup lang="ts">
+import { useMusicStore } from "@/stores"
+
+const musicStore = useMusicStore()
+
+onMounted(() => !(musicStore.playlists.length > 0) ? musicStore.fetchPlaylists() : null)
+</script>
