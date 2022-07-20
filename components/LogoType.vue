3ba497529011329ccf1d7a6b31f7901b819abdad
Hayk Khachatryan <hi@hayk.io>
2022-07-20 14:39:07 -0700

    feat: hello logotype component

---
+++ components/LogoType.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink to="/" class="logo-type">
+    what the playlist
+  </NuxtLink>
+</template>
+
+<style lang="scss">
+.logo-type {
+  @apply btn btn-ghost normal-case text-xl;
+}
+</style>
